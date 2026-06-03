275a18d85d292d46ecec2ec3e8fa1b5f42d76b42
tamimhasan13 <tamimhasan975@gmail.com>
2026-06-03 18:05:18 +0600

    post reducer

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
+import { useReducer } from "react";
+import { initialState, postReducer } from "../reducers/PostReducer";
 
 const HomePage = () => {
   const { auth } = useAuth();
+  const [state,dispatch]=useReducer(postReducer,initialState)
   console.log(auth);
   return (
     <div>
